47006786d3a0bd238d759724f559a155a7fdd31d
AlanYuc <alan.yuc@outlook.com>
2025-03-29 20:25:54 +0800

    添加旋转

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,26 @@ public class PlayerController : MonoBehaviour
     //public Transform transform;
 
     public float moveSpeed;
+    public float mouseSensitivity;
 
     void Start()
     {
         transform.position = new Vector3(-0.2f, transform.position.y, transform.position.z);
         moveSpeed = 1.5f;
+        mouseSensitivity = 800.0f;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        PlayerMove();
+        PlayerView();
+    }
+
+    /// <summary>
+    /// 玩家移动
+    /// </summary>
+    private void PlayerMove()
     {
         float offsetV = Input.GetAxis("Vertical");
         float offsetH = Input.GetAxis("Horizontal");
@@ -26,5 +37,17 @@ public class PlayerController : MonoBehaviour
         Vector3 moveH = transform.right * offsetH * moveSpeed * Time.deltaTime;
         transform.position += moveV;
         transform.position += moveH;
+    }
+
+    /// <summary>
+    /// 玩家视角旋转
+    /// </summary>
+    private void PlayerView()
+    {
+        float offsetX = Input.GetAxis("Mouse X");//获取鼠标水平偏移量
+        float offsetY = Input.GetAxis("Mouse Y");//获取鼠标垂直偏移量
+        //四元数旋转
+        Quaternion rotationX = Quaternion.AngleAxis(offsetX, Vector3.up);//Vector3.up就是绕Y轴的水平旋转
+        transform.rotation *= rotationX;
     }
 }
